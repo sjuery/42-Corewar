f55de68629ba70f8d356365a0f074c9a5ca1d20c
Lunairi <mizukori250@gmail.com>
2018-02-07 18:34:33 -0800

    Save me aneesh

--- src/parse_file.c
+++ src/parse_file.c
@@ -12,10 +12,26 @@
 
 #include <corewar.h>
 
+void	convert_header(t_io *info)
+{
+	int i = -1;
+	while (++i < 8)
+	{
+		info->head.magic[i] = info->header[i];
+	}
+	ft_printf("MAGIC %i - ", info->head.magic);
+	while (++i < 2192)
+	{
+		ft_printf("%c", info->header[i]);
+	}
+	ft_printf("\n");
+}
+
 int	read_input(int fd, t_io *info)
 {
 	if (read(fd, info->header, 2192) <= 0)
 		return (0);
+	convert_header(info);
 	if ((info->bytes = read(fd, info->body, CHAMP_MAX_SIZE)) <= 0)
 		return (0);
 	info->body[info->bytes] = 0;
