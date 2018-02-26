61d91c7c77b9a3de654cf04018ca33f177d36aea
Michael Lu <eggplant@Michaels-iMac.local>
2018-02-26 11:34:43 -0800

    Added skeleton for add with comments

--- src/vm_arg_two.c
+++ src/vm_arg_two.c
@@ -36,10 +36,38 @@ void	vm_aff(t_vm *vm, int i)
 
 void	vm_add(t_vm *vm, int i)
 {
-	(void)vm;
-	(void)i;
+	// we can skip ACB because its always 01010100
+	vm->info[i].index+=2; // skips 2 forward to the first registry
+	// may need aneesh to double check this, using the same functionality we used in st/ld to copy the values stored inside registry
+	int output;
+
+	// this is taking value in first registry * second registry
+	output = (vm->core[vm->info[i].start + vm->info[i].index] * 256) + (vm->core[vm->info[i].start + vm->info[i].index + 1] * 256);
+	// this below part is wrong, will have aneesh fix this up so the output works since its bit wise operands
+	vm->info[i].regs[vm->core[vm->info[i].start + vm->info[i].index + 2]][0] = output;
+	vm->info[i].regs[vm->core[vm->info[i].start + vm->info[i].index + 2]][1] = output;
+	vm->info[i].regs[vm->core[vm->info[i].start + vm->info[i].index + 2]][2] = output;
+	vm->info[i].regs[vm->core[vm->info[i].start + vm->info[i].index + 2]][3] = output;
 	ft_printf("add called");
 }
+
+/*
+ADD .cor
+0454 0102 0304 5403 0201 0454 0103 0302
+9000 0000 0e02 0290 0000 0000 0102 d000
+2a03 
+
+04 = add
+54 = acb
+01 = 1st registry
+02 = 2nd registry
+03 = add 1st + 2nd and place output into this registry
+04 = add
+54 = acb
+03 = 1st
+02 = 2nd
+04 = output
+*/
 
 void	vm_or(t_vm *vm, int i)
 {
