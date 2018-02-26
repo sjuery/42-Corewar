522749d49e8a16db97081d945d8fcc9d27b1b8cd
Michael Lu <eggplant@Michaels-iMac.local>
2018-02-26 11:40:34 -0800

    We did lld not ld, setting up a note for adding %ldx_mod

--- src/vm_arg_three.c
+++ src/vm_arg_three.c
@@ -14,9 +14,38 @@
 
 void	vm_lld(t_vm *vm, int i)
 {
-	(void)vm;
-	(void)i;
-	ft_printf("lld called");
+	char	acb;
+	int		pos;
+	int		ind;
+
+	pos = vm->info[i].index + 1;
+	acb = vm->info[i].body[pos];
+	if (valid_acb(acb, 2, 1, 0))
+	{
+		vm->info[i].index+=2;
+		vm->info[i].regs[vm->core[pos + 5]][0] = vm->core[vm->info[i].start + vm->info[i].index++];
+		vm->info[i].regs[vm->core[pos + 5]][1] = vm->core[vm->info[i].start + vm->info[i].index++];
+		vm->info[i].regs[vm->core[pos + 5]][2] = vm->core[vm->info[i].start + vm->info[i].index++];
+		vm->info[i].regs[vm->core[pos + 5]][3] = vm->core[vm->info[i].start + vm->info[i].index++];
+		ft_printf("lld direct %hhx %hhx %hhx %hhx\n", vm->info[i].regs[vm->core[pos + 5]][0], vm->info[i].regs[vm->core[pos + 5]][1], vm->info[i].regs[vm->core[pos + 5]][2], vm->info[i].regs[vm->core[pos + 5]][3]);
+		vm->info[i].carry++;
+	}
+	else if (valid_acb(acb, 3, 1, 0))
+	{
+		vm->info[i].index+=2;
+		ind = (vm->core[vm->info[i].start + vm->info[i].index] * 256) + vm->core[vm->info[i].start + vm->info[i].index + 1];
+		vm->info[i].regs[vm->core[pos + 3]][0] = vm->core[vm->info[i].start + vm->info[i].index - 2 + ind++];
+		vm->info[i].regs[vm->core[pos + 3]][1] = vm->core[vm->info[i].start + vm->info[i].index - 2 + ind++];
+		vm->info[i].regs[vm->core[pos + 3]][2] = vm->core[vm->info[i].start + vm->info[i].index - 2 + ind++];
+		vm->info[i].regs[vm->core[pos + 3]][3] = vm->core[vm->info[i].start + vm->info[i].index - 2 + ind++];
+		ft_printf("lld indirect %hhx %hhx %hhx %hhx\n", vm->info[i].regs[vm->core[pos + 3]][0], vm->info[i].regs[vm->core[pos + 3]][1], vm->info[i].regs[vm->core[pos + 3]][2], vm->info[i].regs[vm->core[pos + 3]][3]);
+		vm->info[i].carry++;
+	}
+	else
+		ft_printf("ld Burn!\n");
+		// burn cycle
+	//read_acb(vm->info[i].body[vm->info[i].index + 1]);
+	//ft_printf("ld called %i", vm->info[i].body[vm->info[i].index + 1]);
 }
 
 
@@ -34,7 +63,7 @@ void	vm_ldi(t_vm *vm, int i)
 	ft_printf("ldi called");
 }
 
-void	vm_ld(t_vm *vm, int i)
+void	vm_ld(t_vm *vm, int i) // missing %idx_mod
 {
 	char	acb;
 	int		pos;
