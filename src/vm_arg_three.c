fdfdea3f48556c27490d293017609c587ff23560
Lunairi <mizukori250@gmail.com>
2018-02-25 20:33:54 -0800

    Addingp rototype not tested or debugged st

--- src/vm_arg_three.c
+++ src/vm_arg_three.c
@@ -68,17 +68,35 @@ void	vm_ld(t_vm *vm, int i)
 	//ft_printf("ld called %i", vm->info[i].body[vm->info[i].index + 1]);
 }
 
+// 0290 0201
+// 0290 02 0004
+
 void	vm_st(t_vm *vm, int i)
 {
 	char	acb;
+	int		pos;
+	int		ind;
 
-	acb = vm->info[i].body[vm->info[i].index + 1];
+	pos = vm->info[i].index + 1;
+	acb = vm->info[i].body[pos];
 	if (valid_acb(acb, 1, 1, 0))
 	{
-		// copy from reg to reg
+		vm->info[i].regs[vm->core[pos + 2]][0] = vm->info[i].regs[vm->core[pos + 1]][0];
+		vm->info[i].regs[vm->core[pos + 2]][1] = vm->info[i].regs[vm->core[pos + 1]][1];
+		vm->info[i].regs[vm->core[pos + 2]][2] = vm->info[i].regs[vm->core[pos + 1]][2];
+		vm->info[i].regs[vm->core[pos + 2]][3] = vm->info[i].regs[vm->core[pos + 1]][3];
+		ft_printf("%hhx %hhx %hhx %hhx\n", vm->info[i].regs[vm->core[pos + 2]][0], vm->info[i].regs[vm->core[pos + 2]][1], vm->info[i].regs[vm->core[pos + 2]][2], vm->info[i].regs[vm->core[pos + 2]][3]);
+	
 	}
 	else if (valid_acb(acb, 1, 3, 0))
 	{
+		vm->info[i].index+=2;
+		ind = (vm->core[vm->info[i].start + vm->info[i].index] * 256) + vm->core[vm->info[i].start + vm->info[i].index + 1];
+		vm->core[vm->info[i].start + vm->info[i].index - 2 + ind++] = vm->info[i].regs[vm->core[pos + 1]][0];
+		vm->core[vm->info[i].start + vm->info[i].index - 2 + ind++] = vm->info[i].regs[vm->core[pos + 1]][1];
+		vm->core[vm->info[i].start + vm->info[i].index - 2 + ind++] = vm->info[i].regs[vm->core[pos + 1]][2];
+		vm->core[vm->info[i].start + vm->info[i].index - 2 + ind++] = vm->info[i].regs[vm->core[pos + 1]][3];
+		ft_printf("%hhx %hhx %hhx %hhx\n", vm->core[vm->info[i].start + vm->info[i].index - 2 + ind - 4], vm->core[vm->info[i].start + vm->info[i].index - 2 + ind - 3], vm->core[vm->info[i].start + vm->info[i].index - 2 + ind - 2], vm->core[vm->info[i].start + vm->info[i].index - 2 + ind - 1]);
 		// st
 	}
 	else
