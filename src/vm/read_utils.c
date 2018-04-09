aff37c0395da77ca8e01e72cdea625e4b755cf1b
aneeshnazar <aneesh_naz@yahoo.com>
2018-04-09 15:38:48 -0700

    added read_reg2

--- src/vm/read_utils.c
+++ src/vm/read_utils.c
@@ -6,42 +6,46 @@
 /*   By: anazar <anazar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/07 17:46:56 by anazar            #+#    #+#             */
-/*   Updated: 2018/04/07 18:04:25 by anazar           ###   ########.fr       */
+/*   Updated: 2018/04/09 15:38:26 by anazar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <corewar.h>
 
-unsigned char    read_core1(t_vm *vm, unsigned int pos)
+char    read_core1(t_vm *vm, unsigned int pos)
 {
     return (vm->core[pos % MEM_SIZE]);
 }
 
 short   read_core2(t_vm *vm, unsigned int pos)
 {
-    return (((short)vm->core[pos % MEM_SIZE] << 8) | ((short)vm->core[(pos + 1) % MEM_SIZE]));
+    return (vm->core[pos % MEM_SIZE] << 8 + vm->core[(pos + 1) % MEM_SIZE]);
 }
-
 
 int     read_core4(t_vm *vm, unsigned int pos)
 {
-    return ((int)(((unsigned int)vm->core[pos % MEM_SIZE] << 24) |
-        ((unsigned int)vm->core[(pos + 1) % MEM_SIZE] << 16) |
-        ((unsigned int)vm->core[(pos + 2) % MEM_SIZE] << 8) |
-        (unsigned int)vm->core[(pos + 3) % MEM_SIZE]));
+    return (vm->core[pos % MEM_SIZE] << 24 +
+        vm->core[(pos + 1) % MEM_SIZE] << 16 +
+        vm->core[(pos + 2) % MEM_SIZE] << 8 +
+        vm->core[(pos + 3) % MEM_SIZE]);
 }
 
-int     read_reg(t_io *proc, int reg_num)
+int     read_reg2(t_vm *vm, int reg_num)
 {
-    return ((int)(VAL(proc->regs[reg_num])));
+    return (vm->regs[reg_num][3] | (vm->regs[reg_num][2] << 8));
 }
 
-void    write_reg(t_io *proc, int reg_num, int value)
+int     read_reg(t_vm *vm, int reg_num)
 {
-    proc->regs[reg_num][0] = (value >> 24) % 0x100;
-    proc->regs[reg_num][1] = (value >> 16) % 0x100;
-    proc->regs[reg_num][2] = (value >> 8) % 0x100;
-    proc->regs[reg_num][3] = value % 0x100;
+    return (VAL(vm->regs[reg_num]));
+}
+
+void    write_reg(t_vm *vm, int reg_num, int value)
+{
+    vm->regs[reg_num][0] = (value >> 24) % 0x100;
+    vm->regs[reg_num][1] = (value >> 16) % 0x100;
+    vm->regs[reg_num][2] = (value >> 8) % 0x100;
+    vm->regs[reg_num][3] = value % 0x100;
 }
 
 void    write_core(t_vm *vm, unsigned int pos, int value)
