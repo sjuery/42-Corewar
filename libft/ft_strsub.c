589d97e42adbb304e73e202f0fa243c046df89cd
Isamar Hodge <ihodge@e1z4r6p7.42.us.org>
2018-04-11 14:49:51 -0700

    libft fixes

--- libft/ft_strsub.c
+++ libft/ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: anazar <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/06 15:54:07 by anazar            #+#    #+#             */
-/*   Updated: 2017/07/11 10:10:29 by anazar           ###   ########.fr       */
+/*   Updated: 2018/04/11 13:39:56 by ihodge           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@ char		*ft_strsub(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
 	size_t	i;
+	size_t	str_len;
 
+	str_len = ft_strlen(s);
 	if (!s || !(substr = ft_strnew(len)))
 		return (NULL);
 	i = 0;
-	while (i < len)
+	while (i < len && start < str_len)
 		substr[i++] = s[start++];
 	return (substr);
 }
